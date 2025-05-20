c77522d9d7b726b2b39636d77041184ef5a5cf5a
nuram <nuramun28@gmail.com>
2025-05-20 21:41:00 +0300

    add Unit tests

--- src/main/java/somepackage/Main.java
+++
@@ -1,8 +0,0 @@
-package somepackage;
-
-public class Main {
-    public static void main(String[] args) {
-        SomeBean bean = new Injector().inject(new SomeBean());
-        bean.foo(); // Вывод: AC
-    }
-}
